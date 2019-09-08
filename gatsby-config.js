a996cecf2771ced55e5b75e4be369332714e19db
Kim <kimfou@gmail.com>
2019-09-08 17:57:11 +0200

    Convert old site to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `kimf.se`,
+    description: ``,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,6 +28,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`100`, `300`, `400`]
+          },
+        ],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import "../styles/font-icons.scss";
+import "../styles/site.scss";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <h1 className="greeting">
+      <span className="greeting__prefix">hi! i'm <span className="name">kim</span></span>
+    </h1>
+    <div className="text">web &middot; apps &middot; games</div>
+    <ul className="social-network-links">
+      <li><a href="mailto:hello@kimf.se" className="social-network-link icon-mail-alt"></a></li>
+      <li><a href="https://github.com/foke/" className="social-network-link icon-github"></a></li>
+      <li><a href="http://www.linkedin.com/in/fouchenette" className="social-network-link icon-linkedin"></a></li>
+    </ul>
   </Layout>
 )
 
